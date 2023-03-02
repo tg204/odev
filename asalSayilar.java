6a609a0801300214aa46c33be6123bee88d6cc6b
tg204 <tg_204@hotmail.com>
2023-03-02 16:23:16 +0300

    asal sayilar eklendi.

---
+++ asalSayilar.java
@@ -0,0 +1,22 @@
+
+public class asalSayilar{
+    public static void main(String[] args) {
+
+        for(int i = 2; i < 100; i++){
+
+            int sayaci = 0;
+
+            for(int k = 1 ; k <= i ; k++){
+
+                if(i % k == 0){
+                    sayaci++;
+                }
+            }
+            if(sayaci <= 2){
+                System.out.print(i);
+                System.out.print(" ");
+            }
+
+            }
+        }
+    }
