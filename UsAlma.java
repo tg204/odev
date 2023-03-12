3d9f6e103757cf5bf561227689566660c7428523
tg204 <tg_204@hotmail.com>
2023-03-12 16:19:53 +0300

    gelismiş hesap makinası eklendi.

--- UsAlma.java
+++ UsAlma.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
-public class vucutKitleIndex {
 
-	public static void main(String[] args) {
-		Scanner input =new Scanner (System.in);
-		int kg;
-		double m,index;
-		System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz:");
-		m=input.nextDouble();
-		System.out.print("Lütfen kilonuzu giriniz:");
-		kg=input.nextInt();
-		index=kg/(m*m);
-		System.out.print("Vucut kitle indexiniz:"+index);
-			
+public class UsAlma {
+    static  int usAlma(int taban,int us){
+        if(us<1)return 1;
+        else return taban * usAlma(taban,us-1);
+    }
+    public static void main(String[] args) {
+        Scanner tara = new Scanner(System.in);
+        int t,u;
 
-	}
+        System.out.print("Sayının tabanını giriniz: ");
+        t = tara.nextInt();
 
+        System.out.print("Sayının üssünü giriniz: ");
+        u = tara.nextInt();
+
+        System.out.println("Sonuc: " + usAlma(t,u));
+
+
+    }
 }
